89303e5b3842392ac4dfcfff70161d84af08fb21
Harunene <taskinrits@gmail.com>
2023-07-31 23:39:27 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
-// swift-tools-version:5.8
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
         name: "PlaceholderUITextView",
+        platforms: [
+                .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        ],
         products: [
             .library(name: "PlaceholderUITextView", targets: ["PlaceholderUITextView"])
         ],
         dependencies: [
         ],
         targets: [
-            .target(name: "PlaceholderUITextView",path: "Source"),
+            .target(name: "PlaceholderUITextView", path: "Source"),
         ]
 )
